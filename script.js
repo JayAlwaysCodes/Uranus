5e692a1dba4e7932ad2ee835591da441c38d4d9f
JayAlwaysCodes <johnsonjardocs@gmail.com>
2025-01-31 04:27:24 +0100

    all functionalities implemented

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 const fileInput = promptForm.querySelector("#file-input");
 const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
+const themeToggle = document.querySelector("#theme-toggle-btn");
 
 const API_KEY = "AIzaSyBY-rofFqZ4L8292pJgKTbQXlIB-JTedOI";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
@@ -110,7 +111,12 @@ const generateResponse = async (botMsgDiv, retryCount = 0) => {
         chatHistory.push({ role: "model", parts: [{ text: responseText }] });
 
     } catch (error) {
-        console.error("Error generating response:", error);
+        // console.error("Error generating response:", error);
+        textElement.style.color = "#d62939";
+        textElement.textContent = error.name === "AbortError" ? "Response generate stopped." : error.message;
+        botMsgDiv.classList.remove("loading");
+        document.body.classList.remove("bot-responding");
+        scrollToBottom();
 
         // Stop the avatar animation in case of error
         const avatar = botMsgDiv.querySelector(".avatar");
@@ -141,11 +147,11 @@ const generateResponse = async (botMsgDiv, retryCount = 0) => {
 const handleFormSubmit = (e) => {
     e.preventDefault();
     const userMessage = promptInput.value.trim();
-    if(!userMessage) return;
+    if(!userMessage || document.body.classList.contains("bot-responding")) return;
 
     promptInput.value = "";
     userData.message = userMessage;
-    document.body.classList.add("bot-responding")
+    document.body.classList.add("bot-responding", "chats-active")
     fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
 
     //generate user message HTML and add in the chats container
@@ -214,6 +220,41 @@ document.querySelector("#stop-response-btn").addEventListener("click", () => {
         document.body.classList.remove("bot-responding");
     }
 });
+
+//delete chat
+document.querySelector("#delete-chats-btn").addEventListener("click", () => {
+    chatHistory.length = 0;
+    chatsContainer.innerHTML = "";
+    document.body.classList.remove("bot-responding", "chats-active");
+});
+
+
+//handle suggestion clicks
+document.querySelectorAll(".suggestion-item").forEach(item => {
+    item.addEventListener("click", () => {
+        promptInput.value = item.querySelector(".text").textContent;
+        promptForm.dispatchEvent(new Event("submit"));
+    });
+});
+
+//show/hide controls for mobile on prompt input focus
+document.addEventListener("click", ({ target }) => {
+    const wrapper = document.querySelector(".prompt-wrapper");
+    const shouldHide = target.classList.contains("prompt-input") || (wrapper.classList.contains("hide-controks")&&(target.id === "add-file-btn" || target.id === "stop-response-btn"));
+    wrapper.classList.toggle("hide-controls", shouldHide);
+});
+
+//toggle between light and dark mode
+themeToggle.addEventListener("click", () => {
+    const isLightTheme = document.body.classList.toggle("light-theme");
+    localStorage.setItem("themeColor", isLightTheme ? "light_mode" : "dark_mode");
+    themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode";
+});
+
+//set initial theme from local storage
+const isLightTheme = localStorage.getItem("themeColor") === "light_mode";
+document.body.classList.toggle("light-theme", isLightTheme);
+themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode";
 
 promptForm.addEventListener("submit", handleFormSubmit);
 promptForm.querySelector("#add-file-btn").addEventListener("click", () => fileInput.click());
